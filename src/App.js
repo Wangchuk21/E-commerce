6be0231a6ac62ad658f9fc5b5e30218916e8c902
WANGCHUK <wangchukdev@gmail.com>
2022-06-24 12:10:45 +0600

    Updating our app to navigate to gome if user is signed in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes} from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/hompage.component';
 import ShopPage from './pages/shop-page/shopPage.component.jsx';
@@ -46,7 +46,7 @@ class App extends React.Component {
            <Routes>
              <Route path='/' element={<HomePage/>}/>
              <Route path='/shop' element={<ShopPage/>}/>
-             <Route path='/signin' element={<SignInAndSignUpPage/>}/>
+             <Route path='/signin' element={this.props.currentUser ? <Navigate to='/'/> : <SignInAndSignUpPage/>}/>
            </Routes>
       </div>
    );
@@ -54,10 +54,14 @@ class App extends React.Component {
  
 
 }
+
+const mapStateToProps=({user})=>({
+  currentUser: user.currentUser
+})
   
 const mapDispatchToProps = dispatch=>({
   setCurrentUser:user=>dispatch(setCurrentUser(user))
 })
 
-export default connect(null,mapDispatchToProps) (App);
+export default connect(mapStateToProps,mapDispatchToProps) (App);
  
