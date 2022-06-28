49fda4929d4ca06a72c9319984d1af3177a37167
WANGCHUK <wangchukdev@gmail.com>
2022-06-28 12:23:28 +0600

    updating our application with reselesct, replaced cart selectors with memoized selectors

--- src/component/card-icon/card-icon.component.jsx
+++ src/component/card-icon/card-icon.component.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 
 import { connect } from "react-redux";
+import { selectCartItemsCount } from "../../pages/redux/cart/cart.selector";
 
 import { toggleCartHidden } from "../../pages/redux/cart/cart.actions";
 import { ReactComponent as ShoppingIcon } from "../../asset/shopping-bag.svg";
 
 import './card-icon.styles.scss';
 
-const CartIcon= ({toggleCartHidden})=>(
+const CartIcon= ({toggleCartHidden,itemCount})=>(
     <div className="cart-icon" onClick={toggleCartHidden}>
         <ShoppingIcon className="shoping-icon"/>
-        <span className="item-count">0</span>
+        <span className="item-count">{itemCount}</span>
     </div> 
 )
 
@@ -18,7 +19,11 @@ const matchDispatchToProps= dispatch=>({
     toggleCartHidden: ()=> dispatch(toggleCartHidden())
 });
 
+const mapStateToProps=(state)=>({
+    itemCount: selectCartItemsCount(state)
+})
+
  
-export default connect(null,matchDispatchToProps)(CartIcon);
+export default connect(mapStateToProps,matchDispatchToProps)(CartIcon);
 
 
--- src/component/cart-dropdown/cart-dropdown.component.jsx
+++ src/component/cart-dropdown/cart-dropdown.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {connect} from 'react-redux';
 import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.componet";
+import { selectCartItems } from "../../pages/redux/cart/cart.selector";
 import './cart-dropdown.styles.scss'
 
 const CartDropDown=({cartItems})=>(
@@ -15,8 +16,8 @@ const CartDropDown=({cartItems})=>(
     </div>
 )
 
-const mapStateToProps=({cart: {cartItems}})=>({
-    cartItems
+const mapStateToProps=(state)=>({
+    cartItems: selectCartItems(state)
 })
 
 export default connect(mapStateToProps)(CartDropDown);
